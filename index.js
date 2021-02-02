2c8672748ea070eb5b4ec14480a42e70ca194089
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-02 14:47:12 -0500

    user prompts set

--- index.js
+++ index.js
@@ -7,6 +7,53 @@ inquirer.prompt([
             type: 'input',
             message: "Title of project?",
             name: 'title',
-            validation: (value)=>{ if(value){return true} else {return 'Please enter a proper value to continue'}},
-        }
+            // validation: (value)=>{ if(value){return true} else {return 'Please enter a proper value to continue'}},
+        },
+        {
+            type: 'input',
+            message: "Describe your project",
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: "Please enter installation instructions",
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: "Please enter your project’s features, interface, caveats, gotchas, tips, etc.",
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: "Please choose a license relevant to your project",
+            name: 'license',
+            choices: ['MIT License', 'GNU Lesser', 'Mozilla Public', 'Apache', 'The Unlicense'],
+        },
+        {
+            type: 'input',
+            message: "Please provide information on how other users can contribute to this project",
+            name: 'contribute',
+        },
+        {
+            type: 'input',
+            message: "Please provide any tests and necessary examples",
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: "Please enter your GitHub username",
+            name: 'GitHub',
+        },
+        {
+            type: 'input',
+            message: "Please enter your email",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Please provide instructions on how others may reach out to you",
+            name: 'contact',
+        },
+
     ])
