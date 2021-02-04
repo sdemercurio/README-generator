65011c4cb6a729d90b2f9d1a6491847030a3c8d1
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-04 13:20:45 -0500

    rearranging things to utilize the starter files

---
+++ generateMarkdown.js
@@ -0,0 +1,50 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## Table of Contents
+  *[Description]{#description}
+  *[Installation]{#installation}
+  *[Usage]{#usage}
+  *[License]{#license}
+  *[Contribution]{#contribution}
+  *[Tests]{#tests}
+  *[Question]{#questions}
+  
+  ## Description
+  ${data.description}
+  
+  ## Installation
+  ${data.install}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ${data.license}
+  
+  ## Contributions
+  ${data.constribute}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions, please feel free to contact me at
+  ##### Github: [github.com/${data.username}](https://github.com/${data.username})
+  ##### Email: [${data.email}]
+`;
+}
+
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// Ask if this is handled correctly
+const generateMarkdown = require('./generateMarkdown');
+const writeFileAsync = util.promisify(fs.writeFile);
+let file = 'README.md';
+
+const userPrompt = () => {
+ return inquirer.prompt([
+        {
+            type: 'input',
+            message: "Title of project?",
+            name: 'title',
+            // validation: (value)=>{ if(value){return true} else {return 'Please enter a proper value to continue'}},
+        },
+        {
+            type: 'input',
+            message: "Describe your project",
+            name: 'description',
+        },
+        {
+            type: 'editor',
+            message: "Please enter installation instructions",
+            name: 'install',
+        },
+        {
+            type: 'editor',
+            message: "Please enter your project’s features, interface, caveats, gotchas, tips, etc.",
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: "Please choose a license relevant to your project",
+            name: 'license',
+            choices: ['MIT License', 'GNU Lesser', 'Mozilla Public', 'Apache', 'The Unlicense'],
+        },
+        {
+            type: 'input',
+            message: "Please provide information on how other users can contribute to this project",
+            name: 'contribute',
+        },
+        {
+            type: 'input',
+            message: "Please provide any tests and necessary examples",
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: "Please enter your GitHub username",
+            name: 'GitHub',
+        },
+        {
+            type: 'input',
+            message: "Please enter your email",
+            name: 'email',
+        },
+        
+
+    ]);
+
+}
+
+// // TODO: Create a function to initialize app
 function init() {}
 
-// Function call to initialize app
+// // Function call to initialize app
 init();
